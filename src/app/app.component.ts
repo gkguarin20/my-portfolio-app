7e4730bb7df40b0bc025ad97b2ef2ce1a1e22069
Katerine Guarin <giobannaguarin@gmail.com>
2024-01-16 15:05:19 +1100

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { SkillComponent } from './skill/skill.component';
+import { InterestComponent } from './interest/interest.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, HomeComponent, HeaderComponent, NavComponent, AboutComponent,ExperienceComponent,EducationComponent,SkillComponent,InterestComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillComponent } from './skill/skill.component';
+import { EducationComponent } from './education/education.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'header', component: HeaderComponent},
+    { path: 'about', component: AboutComponent},
+    { path: 'experience', component: ExperienceComponent},
+    { path: 'education', component: EducationComponent},
+    { path: 'skill', component: SkillComponent},
+    { path: 'contact', component: ContactComponent},
+    { path: '**', component: HomeComponent, pathMatch: 'full'}
+    
+    
+];
